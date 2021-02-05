80b3c668be97331e7e97079193fe703307e45bd3
inokappa <inokara@gmail.com>
2021-02-05 10:02:28 +0900

    change backend bucket name

--- resources/backend.tf
+++ resources/backend.tf
@@ -5,9 +5,5 @@ provider "aws" {
 terraform {
   required_version = "~> 0.13"
 
-  backend "s3" {
-    bucket = var.BACKEND_BUCKET
-    key    = "terraform-github-actions.terraform.tfstate"
-    region = "ap-northeast-1"
-  }
+  backend "s3" {}
 }
